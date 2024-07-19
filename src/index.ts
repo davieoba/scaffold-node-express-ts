5cce4d4d8b53711293f36ac442e796a6772ff9e7
davieoba <bodunrindavidbond@gmail.com>
2024-07-19 15:50:25 +0100

    feat: app-setup

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
-import App from "./App"
-import { ENVIRONMENTS } from "./config/app.config"
-import Env from "./config/app.keys"
-import connectToDB from "./config/db"
 import { logger } from "./extensions/helpers/logger.helper"
 import validateEnvironmentVariables from "./extensions/utils/env-validator"
 
 validateEnvironmentVariables()
-connectToDB()
-  .then(() => {
-    App.listen(Env.PORT, () => {
-      if (Env.ENVIRONMENT === ENVIRONMENTS.DEV)
-        logger.info(
-          `Express is listening on http://localhost:${Env.PORT}${Env.API_PATH}`
-        )
-    })
-  })
-  .catch(() => logger.error(`DB Connection not successful`))
+// connectToDB()
+//   .then(() => {
+//     App.listen(Env.PORT, () => {
+//       if (Env.ENVIRONMENT === ENVIRONMENTS.DEV)
+//         logger.info(
+//           `Express is listening on http://localhost:${Env.PORT}${Env.API_PATH}`
+//         )
+//     })
+//   })
+//   .catch(() => logger.error(`DB Connection not successful`))
 
 process.on("unhandledRejection", (reason: string, p: Promise<any>) => {
   logger.error("Unhandled Rejection at:\n", p)
